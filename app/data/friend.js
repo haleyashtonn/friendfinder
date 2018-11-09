908b7af485e870fa7b2370bb4fb1e89653bb0689
Haley Murawski <haleyashtonn@gmail.com>
2018-11-08 21:54:58 -0600

    app folder updated

---
+++ app/data/friend.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Chad",
+	"photo" : "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3446927.jpg",
+	"scores" : [1,4,5,5,5,5,2,1,3,5]
+}, {
+	"name" : "Beatrice",
+	"photo" : "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4197877.jpg",
+	"scores" : [5,5,4,1,3,5,4,4,5,5]
+}, {
+	"name" : "Caroline",
+	"photo" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHtYsR6WjKnR3MuOSFmNdUeeyoIM5mtXhW3pESX0mD0pmjLHT0",
+	"scores" : [1,3,2,2,2,1,1,3,2,3]
+}];
+
+
+module.exports = friends;
